5a3ba3322add1401aca416bc3384542efe28e3da
lilly <102229995@qq.com>
2019-09-03 09:57:24 +0800

    加入设计尺寸

--- fitConstant/FitExtension.swift
+++ fitConstant/FitExtension.swift
@@ -12,66 +12,76 @@ import UIKit
 private var kFitConstant = 0
 private var kFitFont = 0
 
+private var designWidth: CGFloat = 1242
+private var designHeight: CGFloat = 2208
+
 private func scaleVertical(_ v: CGFloat) -> CGFloat {
-    return UIScreen.main.bounds.height / 667 * v
+	return (UIScreen.main.bounds.height * UIScreen.main.scale / designHeight) * v
 }
 
 private func scaleHorizontal(_ v: CGFloat) -> CGFloat {
-    return UIScreen.main.bounds.width / 375 * v
+	return (UIScreen.main.bounds.width * UIScreen.main.scale / designWidth) * v
+}
+
+public class FitExtension {
+	static func register(width: CGFloat, height: CGFloat) {
+		designWidth = width
+		designHeight = height
+	}
 }
 
 extension NSLayoutConstraint {
-
-    @IBInspectable var fitConstant: Bool {
-        set {
-            if newValue {
-                switch firstAttribute {
-                case .left: fallthrough
-                case .right: fallthrough
-                case .leading: fallthrough
-                case .trailing: fallthrough
-                case .width: fallthrough
-                case .leftMargin: fallthrough
-                case .rightMargin: fallthrough
-                case .leadingMargin: fallthrough
-                case .trailingMargin:
-                    constant = scaleHorizontal(constant)
-
-                case .top: fallthrough
-                case .bottom: fallthrough
-                case .height: fallthrough
-                case .lastBaseline: fallthrough
-                case .firstBaseline: fallthrough
-                case .topMargin: fallthrough
-                case .bottomMargin:
-                    constant = scaleVertical(constant)
-
-                case .notAnAttribute: fallthrough
-                default: break
-                }
-
-            }
-            objc_setAssociatedObject(self, &kFitConstant, newValue, .OBJC_ASSOCIATION_ASSIGN)
-        }
-        get {
-            return objc_getAssociatedObject(self, &kFitConstant) as! Bool
-        }
-    }
-
+	
+	@IBInspectable var fitConstant: Bool {
+		set {
+			if newValue {
+				switch firstAttribute {
+					case .left: fallthrough
+					case .right: fallthrough
+					case .leading: fallthrough
+					case .trailing: fallthrough
+					case .width: fallthrough
+					case .leftMargin: fallthrough
+					case .rightMargin: fallthrough
+					case .leadingMargin: fallthrough
+					case .trailingMargin:
+						constant = scaleHorizontal(constant)
+					
+					case .top: fallthrough
+					case .bottom: fallthrough
+					case .height: fallthrough
+					case .lastBaseline: fallthrough
+					case .firstBaseline: fallthrough
+					case .topMargin: fallthrough
+					case .bottomMargin:
+						constant = scaleVertical(constant)
+					
+					case .notAnAttribute: fallthrough
+					default: break
+				}
+				
+			}
+			objc_setAssociatedObject(self, &kFitConstant, newValue, .OBJC_ASSOCIATION_ASSIGN)
+		}
+		get {
+			return objc_getAssociatedObject(self, &kFitConstant) as! Bool
+		}
+	}
+	
 }
 
 extension UILabel {
-    
-    @IBInspectable var isFitFont: Bool {
-        set {
-            if newValue {
-                font = UIFont.systemFont(ofSize: scaleHorizontal(font.pointSize))
-            }
-            objc_setAssociatedObject(self, &kFitFont, newValue, .OBJC_ASSOCIATION_ASSIGN)
-        }
-        get {
-            return objc_getAssociatedObject(self, &kFitFont) as! Bool
-        }
-    }
-
+	
+	@IBInspectable var isFitFont: Bool {
+		set {
+			if newValue {
+				font = UIFont.systemFont(ofSize: scaleHorizontal(font.pointSize))
+			}
+			objc_setAssociatedObject(self, &kFitFont, newValue, .OBJC_ASSOCIATION_ASSIGN)
+		}
+		get {
+			return objc_getAssociatedObject(self, &kFitFont) as! Bool
+		}
+	}
+	
 }
---
+++ fitConstantDemo/fitConstant.h
@@ -0,0 +1,19 @@
+//
+//  fitConstant.h
+//  fitConstant
+//
+//  Created by yang li on 2019/9/2.
+//  Copyright © 2019 lilly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for fitConstant.
+FOUNDATION_EXPORT double fitConstantVersionNumber;
+
+//! Project version string for fitConstant.
+FOUNDATION_EXPORT const unsigned char fitConstantVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fitConstant/PublicHeader.h>
+
+
